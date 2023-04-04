060ca6a05bf72d5e5ed2db7ea598628d9dedd1b2
Emmanuel HEMERIT <emmanuel@hemer.it>
2023-04-04 22:37:56 +0200

    decoration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
 import QuoteBox from './QuoteBox';
+import logo from "./assets/Kaamelott_Logo.png";
+
 function App() {
   return (
     <div className="App">
+      <div id="titles">
+        <h1>Random Quote Machine</h1>
+        <div id="logo-container">
+          <img src={logo} alt="logo Kaamelott" />
+        </div>
+      </div>
       <QuoteBox></QuoteBox>
     </div>
   );
